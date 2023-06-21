386847f4e1c859f45a0ddbc0f110c0bebd6d3555
pvkog <87erumog@gmail.com>
2023-06-21 11:38:10 +0300

    Added modal window and title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,19 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { HttpClientModule } from '@angular/common/http';
 import { GlobalErrorComponent } from './components/global-error/global-error.component';
-
+import { FormsModule } from '@angular/forms';
+import { FilterProductsPipe } from './pipes/filter-products.pipe';
+import { ModalComponent } from './components/modal/modal.component';
+import { CreateProductComponent } from './components/create-product/create-product.component';
 @NgModule({
   declarations: [
-    AppComponent, ProductComponent, GlobalErrorComponent
+    AppComponent, ProductComponent, GlobalErrorComponent, FilterProductsPipe, ModalComponent, CreateProductComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
