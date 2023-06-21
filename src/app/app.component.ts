4b0babbdc64139f0c5e68fb3096091a6166c2768
pvkog <87erumog@gmail.com>
2023-06-21 08:43:21 +0300

    50% Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+import { ProductService } from './services/products.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular_education';
+export class AppComponent implements OnInit{
+  title = 'angular education';
+  products:IProduct[]= []
+  loading=false;
+products$:Observable<IProduct[]>
+
+  constructor(private productsService: ProductService){
+
+  }
+  ngOnInit():void{
+    this.loading =true
+    this.products$= this.productsService.getAll()
+    this.productsService.getAll().subscribe(products =>{
+      this.products = products   
+      this.loading = false   
+    })
+  }
 }
---
+++ src/app/services/products.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core'
+import {HttpClient, HttpParams} from '@angular/common/http'
+import { IProduct } from '../models/product'
+import {delay, Observable } from 'rxjs'
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProductService{
+    constructor(private http: HttpClient){
+
+    }
+    getAll(): Observable<IProduct[]>{
+        return this.http.get<IProduct[]>('https://fakestoreapi.com/products', {
+            params: new HttpParams({
+                fromObject:{limit:5}
+            })
+        }).pipe(
+            delay(2000)
+        )
+    }
+}
